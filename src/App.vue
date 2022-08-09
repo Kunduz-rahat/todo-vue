950974c3a248ea93a5c18fcd54f74522161cd0d6
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-08-09 17:55:33 +0600

    add functional todo_form

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,76 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ <div>
+  <form @submit.prevent>
+ <input v-bind:value="text"
+ 
+ @input="text=$event.target.value"
+ />
+  <button @click="createTodo">Добавить задачу</button>
+  </form>
+ 
+<div v-for="todo in todos" :key="todo.id">
+
+<input type="checkbox" id="checkbox" v-model="checked" onchange="toggleTodo" />
+<label for="checkbox">{{ checked }}</label>
+<span>{{todo.text}}</span>
+<button @click="removeTodo">Delete</button>
+</div> 
+<button @click="isActive">Активные</button>    
+<button @click="all">Все</button>
+<button @click="isCompleted">Завершенные</button>
+ </div>
 </template>
+<script>
+import axios from 'axios'
+export default{
+  data(){
+    return {
+      todos:[],
+    
+    }
+  },
+  methods:{
+createTodo(){
+const newTodo={
+  id:Date.now(),
+  text:this.text,
+  active:false
+}
+this.todos.push(newTodo)
+this.text=""
+},
+removeTodo(id){
+this.todos.splice(id, 1)
+console.log(id)
+},
+toggleTodo(){
+this.active=!this.active
+},
+all(){
 
+},
+isActive(){
+
+},
+isCompleted(){
+
+},
+async fetchTodos(){
+      try{
+const response = await axios.get('https://my-json-server.typicode.com/falk20/demo/todos')
+this.todos=response.data
+console.log(response)
+      }catch(e){
+        alert('Error')
+      }
+    }
+    
+  },
+  mounted(){
+      this.fetchTodos()
+    }
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
